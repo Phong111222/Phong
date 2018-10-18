7b92323e467c41edb4f8da4cd382a6ac2ec22657
Phong111222 <44263091+Phong111222@users.noreply.github.com>
2018-10-18 23:19:17 +0700

    first commit

---
+++ lab 2/lab2.cpp
@@ -0,0 +1,146 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+	for(int i=1;i<=29;i++)
+	{
+		
+		switch(i)
+		{
+			case 5:
+			case 6:
+			case 8:
+			case 9:
+			case 11:
+			case 12:
+			case 17:
+			case 20:
+			case 21:
+			case 22:
+			case 18:
+			case 24:
+			case 25:
+				cout<<" ";
+				break;
+			default:
+				cout<<"*";
+		}
+	}
+	cout<<endl;
+	for(int i=1;i<=29;i++)
+	{
+		
+		switch(i)
+		{
+			case 2:
+			case 3:
+			case 5:
+			case 6:
+			case 8:
+			case 9:
+			case 11:
+			case 12:
+			case 14:
+			case 15:
+			case 17:
+			case 18:
+			case 21:
+			case 22:
+			case 24:
+			case 25:
+			case 27:
+			case 28:
+			case 29:
+				cout<<" ";
+				break;
+			default:
+				cout<<"*";
+		}
+	}
+	cout<<endl;
+	for(int i=1;i<=29;i++)
+	{
+		
+		switch(i)
+		{
+			case 5:
+			case 6:
+			case 11:
+			case 12:
+			case 14:
+			case 15:
+			case 17:
+			case 18:
+			case 20:
+			case 22:
+			case 24:
+			case 25:
+			case 27:
+			case 28:
+			case 29:
+				cout<<" ";
+				break;
+			default:
+				cout<<"*";
+		}
+	}
+	cout<<endl;
+	for(int i=1;i<=30;i++)
+	{
+		
+		switch(i)
+		{
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+			case 8:
+			case 9:
+			case 11:
+			case 12:
+			case 14:
+			case 15:
+			case 17:
+			case 18:
+			case 20:
+			case 21:
+			case 24:
+			case 25:
+			case 27:
+				cout<<" ";
+				break;
+			default:
+				cout<<"*";
+		}
+	}	
+	cout<<endl;
+	for(int i=1;i<=29;i++)
+	{
+		
+		switch(i)
+		{
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+			case 8:
+			case 9:
+			case 11:
+			case 12:
+			case 17:
+			case 18:
+			case 20:
+			case 21:
+			case 22:
+			case 24:
+			case 25:
+				cout<<" ";
+				break;
+			default:
+				cout<<"*";
+		}
+	}	
+}
